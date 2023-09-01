01281866fa5996d6c0e92f37a72bc4bc74bc6a9e
Ahmed Bouregba <abouregb@e3r9p3.1337.ma>
2023-09-01 17:42:56 +0100

    l

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,13 @@
 
+#include "main.h"
 
-
-#include <unistd.h>
-
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int ac, char **av)
 {
     int i;
