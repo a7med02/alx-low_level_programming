93a0a838ef575355f7ab5408e6511c149b94167b
Ahmed Bouregba <abouregb@e3r7p1.1337.ma>
2023-09-01 23:33:28 +0100

    h

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - prints the minimum number.
+* of coins to make change for an amount of money.
+*@argc : the number of argiments.
+*@argv : the array of argiments.
+* Return: if success return o else return 1.
+*/
+int main(int argc, char *argv[])
+{
+    
+}
