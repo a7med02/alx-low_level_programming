bfe2adbbf656dbef54d7c3c0ce0b7629cdf9e53a
Ahmed Bouregba <abouregb@e3r4p14.1337.ma>
2023-09-05 10:13:12 +0100

    vd

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* function that prints a string.
+*
+* Return: not return anythings.
+*/
+void _print_rev_recursion(char *s)
+{
+    
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,8 @@
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
+
+#include <unistd.h>
+void _puts_recursion(char *s);
+void _putchar(char c);
+
+#endif
