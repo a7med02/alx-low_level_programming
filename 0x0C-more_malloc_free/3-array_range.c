dc86f4b0575a3e378ee6859acee8a12aada20142
medex <medex@gmail.com>
2023-09-07 21:01:53 +0100

    d

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
 /**
-* 
+*array_range-function that return numbers form min to max.
+*@min:mini value.
+*@max:the max value.
+*Return:array of integerse from started from min to max value.
 */
 int *array_range(int min, int max)
 {
-    int i = 0;
-    int *arr;
-    if (min > max)
-        return (NULL);
-    arr = malloc(sizeof(int) * (max - min));
-    if (!arr)
-        return (NULL);
-    while(min <= (max))
-        arr[i++] = min++;
-    return (arr);
+int i = 0;
+int *arr;
+if (min > max)
+	return (NULL);
+arr = malloc(sizeof(int) * (max - min));
+if (!arr)
+	return (NULL);
+while (min <= (max))
+	arr[i++] = min++;
+return (arr);
 }
