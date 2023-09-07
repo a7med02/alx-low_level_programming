b7a02daeca8e6f85b1a50681e95ee184d4442c08
medex <medex@gmail.com>
2023-09-07 17:15:09 +0100

    D

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,29 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-*  function that concatenates two strings.
-* s, followed by the first n bytes of s2, and null terminated.
-*  return : NULL If the function fails.
+*string_nconcat - function that concatenates two strings.
+* @s1 : array
+* @s2 : array 2.
+* @n : number of byt that take from s2;
+* Return: pointer to the address of the memory block
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int i = 0;
-    unsigned int s = 0;
-    char *rt;
-    while(s1[i])
-        i++;
-    rt = malloc(sizeof(char) * (i + n +1));
-    if (!rt)
-        return (NULL);
-    rt[i + n] = '\0';
-    i = 0;
-    while(s1[i++])
-    {
-        rt[i-1] = s1[i-1];
-    }
-    s = i-1;
-    i = 0;
-    while (s2[i] && i < n)
-        rt[s++] = s2[i++];
-    return (rt);
+unsigned int i = 0;
+unsigned int s = 0;
+char *rt;
+while (s1[i])
+	i++;
+rt = malloc(sizeof(char) * (i + n + 1));
+if (!rt)
+	return (NULL);
+rt[i + n] = '\0';
+i = 0;
+while (s1[i++])
+{
+	rt[i - 1] = s1[i - 1];
+}
+s = i - 1;
+i = 0;
+while (s2[i] && i < n)
+	rt[s++] = s2[i++];
+return (rt);
 }
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
-*  
+* _calloc-funtion that allocate a number of byte and set it to 0.
+*@nmemb: ...
+* @size: ... 
+* Return: a block of memory set it to 0 if sucsseful. 
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    char *block;
-    unsigned int i = 0;
-
-    if(nmemb == 0 || size == 0)
-        return (NULL);
-    block = malloc(nmemb * size);
-    if(!block)
-        return (NULL);
-    while(i < (nmemb * size))
-        block[i++] = 0;
-    return (block);
+char *block;
+unsigned int i = 0;
+if (nmemb == 0 || size == 0)
+	return (NULL);
+block = malloc(nmemb * size);
+if (!block)
+	return (NULL);
+while (i < (nmemb * size))
+	block[i++] = 0;
+return (block);
 }
