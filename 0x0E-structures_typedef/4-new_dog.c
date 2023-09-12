d9250dd34bb635a7c5582c02473a7f06acf37279
Ahmed Bouregba <abouregb@e3r4p1.1337.ma>
2023-09-12 22:28:44 +0100

    test

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,20 +11,20 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p;
-	p = malloc(sizeof(dog_t));
-	if (p == NULL)
-		return (NULL);
-	if (name == NULL)
-	{
-		return (NULL);
-	}
-	if (owner == NULL)
-	{
-		return (NULL);
-	}
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
-	return (p);
+dog_t *p;
+p = malloc(sizeof(dog_t));
+if (p == NULL)
+	return (NULL);
+if (name == NULL)
+{
+	return (NULL);
+}
+if (owner == NULL)
+{
+	return (NULL);
+}
+p->name = name;
+p->age = age;
+p->owner = owner;
+return (p);
 }
