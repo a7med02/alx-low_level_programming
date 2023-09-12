e85e6c086d61996ec2ff3720f86ef9c7a9f2e12c
Ahmed Bouregba <abouregb@e3r4p1.1337.ma>
2023-09-12 21:53:21 +0100

    sd

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,13 +1,35 @@
 #include "dog.h"
+#include <stdlib.h>
 
+/**
+ * new_dog - entry point
+ * @name: string from main, name of pet
+ * @age: number from main, age of pet
+ * @owner: string from main, owner of pet
+ * Return: p
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *dog;
-    dog = malloc(sizeof(dog_t));
-    if (!dog)
-        return (NULL);
-    dog->age = age;
-    dog->name = name;
-    dog->owner = owner;
-    return (dog);
+	dog_t *p;
+	/* reserving memory to struct*/
+	p = malloc(sizeof(dog_t));
+	if (p == NULL)
+		return (NULL);
+	/* Cpunting name pointer*/
+	if (name == NULL)
+	{
+		free(p);
+		free(owner);
+		return (NULL);
+	}
+	if (owner == NULL)
+	{
+		free(p);
+		free(name);
+		return (NULL);
+	}
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return (p);
 }
