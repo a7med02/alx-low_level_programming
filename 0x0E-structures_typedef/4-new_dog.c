4d7187843d15f5121e5ff7bad8f407a00b0d1efe
Ahmed Bouregba <abouregb@e3r4p1.1337.ma>
2023-09-12 22:26:54 +0100

    test

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,22 +11,15 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *p;
-/* reserving memory to struct*/
 p = malloc(sizeof(dog_t));
 if (p == NULL)
 	return (NULL);
-/* Cpunting name pointer*/
 if (name == NULL)
 {
-	// free(p);
-    // if (owner)
-	//     free(owner);
 	return (NULL);
 }
 if (owner == NULL)
 {
-	// free(p);
-	// free(name);
 	return (NULL);
 }
 p->name = name;
