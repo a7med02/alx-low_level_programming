69d766a9dcfcfc5cd1f0b49497414ff2a754614e
Ahmed Bouregba <abouregb@e3r4p1.1337.ma>
2023-09-12 23:03:13 +0100

    test

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,44 @@
 #include "dog.h"
 
 /**
+ * ft_strlen - copy a string.
+ * @s: a string to be counted.
+ * Return: string length.
+*/
+int	ft_strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*s)
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * ft_strcpy - copy a string.
+ * @s: a string to be copied.
+ * Return: new string.
+*/
+char *ft_strcpy(char *s)
+{
+	char *d = malloc(sizeof(char ) * (ft_strlen(s) + 1));
+	int i = 0;
+
+	while (*s)
+	{
+		d[i] = *s;
+		i++;
+		s++;
+	}
+	d[i] = '\0';
+	return (d);
+}
+
+/**
 * new_dog - entry point
 * @name: string from main, name of pet
 * @age: number from main, age of pet
@@ -30,14 +68,14 @@ if (!owner)
 	free(name);
 	return (NULL);
 }
-p->name = (name);
+p->name = ft_strcpy(name);
 if (!p->name)
 {
 	free(p);
 	return (NULL);
 }
 p->age = age;
-p->owner = (owner);
+p->owner = ft_strcpy(owner);
 if (!p->owner)
 {
 	free(p);
