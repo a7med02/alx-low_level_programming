c6f43c5d9f1df4c66a55c55963e101ffa18146b7
Ahmed Bouregba <abouregb@e3r4p1.1337.ma>
2023-09-12 22:00:17 +0100

    sd

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,34 +2,34 @@
 #include <stdlib.h>
 
 /**
- * new_dog - entry point
- * @name: string from main, name of pet
- * @age: number from main, age of pet
- * @owner: string from main, owner of pet
- * Return: p or null if have not a new dog
- */
+* new_dog - entry point
+* @name: string from main, name of pet
+* @age: number from main, age of pet
+* @owner: string from main, owner of pet
+* Return: p
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p;
-	/* reserving memory to struct*/
-	p = malloc(sizeof(dog_t));
-	if (p == NULL)
-		return (NULL);
-	/* Cpunting name pointer*/
-	if (name == NULL)
-	{
-		free(p);
-		free(owner);
-		return (NULL);
-	}
-	if (owner == NULL)
-	{
-		free(p);
-		free(name);
-		return (NULL);
-	}
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
-	return (p);
+dog_t *p;
+/* reserving memory to struct*/
+p = malloc(sizeof(dog_t));
+if (p == NULL)
+	return (NULL);
+/* Cpunting name pointer*/
+if (name == NULL)
+{
+	free(p);
+	free(owner);
+	return (NULL);
+}
+if (owner == NULL)
+{
+	free(p);
+	free(name);
+	return (NULL);
+}
+p->name = name;
+p->age = age;
+p->owner = owner;
+return (p);
 }
