1b1b49c37e914ee6fcde4745e7272c0d2866d925
medex <medex@gmail.com>
2023-09-14 14:12:18 +0100

    f

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,9 +13,9 @@ int sum_them_all(const unsigned int n, ...)
 	int sum;
 	va_list list;
 
-	va_start(list, n);
 	if (n == 0)
 		return (0);
+	va_start(list, n);
 	for (i = 0, sum = 0; i < n; i++)
 		sum += va_arg(list, int);
 
